509d5d62bcac45d4a1967008dd56806ce15274e3
Promise-Digitals <promiseedeh025@gmail.com>
2024-09-02 12:52:30 +0100

    completed link scroll

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './Navbar.css';
+import { Link, animateScroll as scroll } from 'react-scroll';
 
 const Navbar = () => {
 
@@ -16,12 +17,12 @@ const Navbar = () => {
         <nav className={`container ${sticky ? 'teal-nav' : '' }`}>
             <img src={('assets/logo.png')} className='logo' />
             <ul>
-                <li>Home</li>
-                <li>Program</li>
-                <li>About Us</li>
-                <li>Campus</li>
-                <li>Testimonials</li>
-                <li><button className='btn'>Contact Us</button></li>
+                <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
+                <li><Link to='programs' smooth={true} offset={-260} duration={500}>Program</Link></li>
+                <li><Link to='about' smooth={true} offset={-150} duration={500}>About Us</Link></li>
+                <li><Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
+                <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
+                <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact Us</Link></li>
             </ul>
         </nav>
     )
